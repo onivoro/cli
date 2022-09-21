61d1d7e0aa0c70d409e4e074490fa57b8029f947
icedlee337 <lee.e.norris@gmail.com>
2022-09-21 17:32:36 -0400

    chore: first commit worx

--- apps/cli/src/main.ts
+++ apps/cli/src/main.ts
@@ -1,22 +1,10 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+#! /usr/bin/env node
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
+import { CommandFactory } from 'nest-commander';
+import { AppCliModule } from './app/app-cli.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  await CommandFactory.run(AppCliModule);
 }
 
-bootstrap();
+void bootstrap();
