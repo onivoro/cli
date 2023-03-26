32e7377ff6cbc75dc9b902d0596e77981b044be6
Lee Norris <lee.norris@arcadia.io>
2023-03-26 09:58:58 -0500

    chore: idk

--- libs/lambda/src/lib/update-function.ts
+++ libs/lambda/src/lib/update-function.ts
@@ -2,6 +2,6 @@ import { opts } from "@oni/util";
 import { execSync } from "child_process";
 
 export function updateFunction(functionName: string, region: string, zip: string) {
-    const cmd = `aws lambda create-function --function-name "${functionName}" --region ${region} --handler "main.handler" --package-type Zip --zip-file "fileb://${zip}" --publish`;
+    const cmd = `aws lambda update-function-code --region ${region} --function-name "${functionName}" --zip-file "fileb://${zip}" --publish`;
     execSync(cmd, opts)
 }
